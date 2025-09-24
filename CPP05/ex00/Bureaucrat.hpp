0e724d9c335e335f43bfcaf9ff4794d9db576fdc
Mota494 <migueloureiromota@gmail.com>
2025-09-24 14:00:24 +0100

    ex00 done

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -3,6 +3,12 @@
 #include <exception>
 #include <iostream>
 
+#define GREEN "\033[38;5;83m"
+#define YELLOW "\033[38;5;220m"
+#define RED "\033[38;5;196m"
+#define PRETTY "\033[38;5;69m"
+#define RESET "\033[0m"
+
 class Bureaucrat
 {
 	private:
@@ -11,14 +17,23 @@ class Bureaucrat
 	public:
 		std::string getName() const;
 		int			getGrade() const;
+		
 		Bureaucrat(const std::string &name, int grade);
 		~Bureaucrat();
 		Bureaucrat(const Bureaucrat &o_bureau);
 		Bureaucrat& operator = (const Bureaucrat &o_bureau);
-
+		
+		void	incGrade();
+		void	decGrade();
 		class GradeToHigh : public std::exception
+		{
+			const char *what() const throw();
+		};
+
+		class GradeToLow : public std::exception
 		{
 			const char *what() const throw();
 		};
 };
 
+std::ostream &operator<<(std::ostream &os ,const Bureaucrat &o_bureau);
