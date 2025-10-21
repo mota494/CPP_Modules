fbf8540fdb460b900f03ded09ab2b2c6b4062130
Mota494 <migueloureiromota@gmail.com>
2025-10-21 14:50:21 +0100

    CPP06 done?

---
+++ CPP06/ex02/Base.cpp
@@ -0,0 +1,55 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base::~Base(){}
+
+Base *generate(void)
+{
+	srand (time(NULL));
+	int	chance = rand() % 3;
+	
+	if (chance == 0)
+		return (new A());
+	else if (chance == 1)
+		return (new B());
+	else
+		return (new C());
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << "A * was generated" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B * was generated" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C * was generated" << std::endl;
+	else
+		std::cout << "Unknown type" << std::endl;
+}
+
+void identify(Base &p) {
+	try {
+		(void)dynamic_cast<A &>(p);
+		std::cout << "A & was generated" << std::endl;
+		return;
+	} catch (const std::exception &e) {
+  
+	}
+	try {
+		(void)dynamic_cast<B &>(p);
+		std::cout << "B & was generated" << std::endl;
+		return;
+	} catch (const std::exception &e) {
+	
+	}
+	try {
+		(void)dynamic_cast<C &>(p);
+		std::cout << "C & was generated" << std::endl;
+		return;
+	} catch (const std::exception &e) {
+		std::cout << "Unknown type" << std::endl;
+	}
+}
