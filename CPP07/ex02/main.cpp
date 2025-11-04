2ea9bfe2774b72c16fe3a7509e97847afa383287
Mota494 <migueloureiromota@gmail.com>
2025-11-04 15:46:19 +0000

    CPP07 done

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -4,5 +4,17 @@ int main(void)
 {
 	Array<int> new_arr(9);
 
-	std::cout << new_arr[8];
+	new_arr[8] = 1;
+	std::cout << new_arr[8] << std::endl;
+	std::cout << new_arr.size() << std::endl;
+	
+	try 
+	{
+		Array<int> err(2);
+		std::cout << err[4];
+	} 
+	catch (std::exception &e) 
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
